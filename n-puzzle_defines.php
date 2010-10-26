10dc5e16849233c8551a2e665aba25f76d89e113
abhirama <none@none>
2010-10-26 13:57:42 +0530

    To reflect the change in app url made in fb

--- n-puzzle_defines.php
+++ n-puzzle_defines.php
@@ -1,5 +1,4 @@
 <?php 
 	define("APP_NAME", "n-puzzle");
-	define("APP_URL", "http://apps.facebook.com/npuzzle/");
-	define("FACEBOOK_INVITE_CONTENT", "<fb:name uid=\"%s\" firstnameonly=\"true\" shownetwork=\"false\"/> wants you to try <a href=\"http://apps.facebook.com/\">n-puzzle</a>.");
+	define("APP_URL", "http://apps.facebook.com/n-puzzle/");
 ?>
